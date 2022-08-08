af15dd5abc0d03300baee0953841a34fb78ffade
JeongsolKim <wjdthf3927@gmail.com>
2022-08-08 23:23:45 +0900

    implement dataloader for png images

--- dataloader.py
+++ dataloader.py
@@ -1,16 +1,32 @@
+import os
+from glob import glob
+from PIL import Image
 from torch.utils.data import DataLoader
 from torchvision.datasets import VisionDataset
+from torchvision import transforms
 
 class CustomDataset(VisionDataset):
-    def __init__(self, root: str):
+    def __init__(self, root: str, train:bool, transform):
         super().__init__(root=root)
+        self.transform = transform
+        stage = "train" if train else "valid"
+        self.image_paths = glob(os.path.join(root, stage, '*.png'))
 
     def __len__(self):
-        return len(self.images)
+        return len(self.image_paths)
 
     def __getitem__(self, index: int):
-        return self.images[index]
+        img_path = self.image_paths[index]
+        image = Image.open(fp=img_path).convert('RGB')
 
+        if self.transform is not None:
+            image = self.transform(image)
 
-def get_loader(root, train):
-    pass
+        return image
+
+def get_valid_loader(root, batch_size):
+    transform = transforms.Compose([
+                transforms.ToTensor()])
+    dataset = CustomDataset(root, False, transform)
+    data_loader = DataLoader(dataset, batch_size)
+    return data_loader
---
+++ tests/test_dataloader.py
@@ -0,0 +1,17 @@
+from dataloader import get_valid_loader, CustomDataset
+
+def test_loader_data_shape():
+    root = '/data/FFHQ/'
+    batch_size = 2
+
+    loader = get_valid_loader(root, batch_size)
+    image = next(iter(loader))
+    assert image.shape == (batch_size, 3, 256, 256)
+
+def test_loader_data_scale():
+    root = '/data/FFHQ/'
+    batch_size = 1
+
+    loader = get_valid_loader(root, batch_size)
+    image = next(iter(loader))
+    assert image.min() >= 0.0 and image.max() <= 1.0
