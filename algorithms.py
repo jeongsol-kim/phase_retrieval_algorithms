30d297bfa891bc1d68d3b94e1536c63279acae77
JeongsolKim <wjdthf3927@gmail.com>
2022-08-09 13:53:15 +0900

    ER implemented (Require check)

--- algorithms.py
+++ algorithms.py
@@ -1,6 +1,6 @@
-from typing import Tuple
-import numpy as np
+from typing import Tuple, Callable
 import torch
+from tqdm import tqdm
 
 import utils
 
@@ -14,21 +14,32 @@ def register_algorithm(name: str):
         return func
     return wrapper
 
-def get_algorithm(name: str):
+def get_algorithm(name: str) -> Callable:
     if __ALGORITHMS__.get(name, None) is None:
         raise NameError(f"Name {name} is not defined.")
     return __ALGORITHMS__[name]
 
 @register_algorithm(name='ER')
-def error_reduction_algorithm(amplitude: torch.Tensor, iteration: int, start_domain: str = 'fourier'):
+def error_reduction_algorithm(amplitude: torch.Tensor, iteration: int):
     padded_amplitude, support = generate_support(amplitude)
     init_phase = generate_random_phase(padded_amplitude, support)
 
-    for i in range(iteration):
-        apply_image_constraint()
-        apply_fourier_constraint()
+    fft_obj = utils.combine_amplitude_phase(padded_amplitude, init_phase)
+    obj = utils.ifft2d(fft_obj)
+
+    pbar = tqdm(range(iteration), miniters=100)
+
+    for i in pbar:
+        pbar.set_description(f"Iteration {i+1}", refresh=False)
+        obj = apply_image_constraint(obj, support)
+        fft_obj = utils.fft2d(obj)
+        fft_obj = apply_fourier_constraint(fft_obj, padded_amplitude)
+        obj = utils.ifft2d(fft_obj)
+
     
-    
+    reconstructed = utils.crop_center_half(obj)
+    return reconstructed
+
 
 @register_algorithm(name='HIO')
 def hybrid_input_and_output_algorithm(amplitude: torch.Tensor, iteration: int, start_domain: str = 'fourier'):
@@ -54,12 +65,16 @@ def generate_support(amplitude: torch.Tensor) -> Tuple[torch.Tensor, torch.Tenso
     return padded_amplitude, support
 
 
-def apply_image_constraint(amplitude, phase, support):
+def apply_image_constraint(obj, support):
+    obj = obj * support
+    return obj
+
+def apply_image_constraint_hio(obj, support):
     pass
 
-
-def apply_fourier_constraint(amplitude, phase, measured_amplitude):
-    pass
+def apply_fourier_constraint(fft_obj, measured_amplitude):
+    substituted_obj = substitute_amplitude(fft_obj, measured_amplitude)
+    return substituted_obj
 
 
 def substitute_amplitude(complex_obj: torch.Tensor, measured_amplitude: torch.Tensor) -> torch.Tensor:
